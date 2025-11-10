c81f1d5f55f2d41d114f8c469a350a26ed3e773a
Manoj7413 <kumharmanoj303@gmail.com>
2025-11-10 23:42:59 +0530

    final3

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,16 @@
 import { defineConfig } from "vite";
+import { resolve } from "path";
 
 export default defineConfig({
+  root: "client",
   build: {
-    outDir: "dist",
+    outDir: "../dist/client",
     emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "client/index.html"),
+      },
+    },
   },
   server: {
     port: 3000,
